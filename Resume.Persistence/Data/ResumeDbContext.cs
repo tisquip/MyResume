9b0635570ebd4bfc24ae54f84d86694f47a88f95
Software Architects of Southern Africa (SASA) <soarsoaf@outlook.com>
2020-11-30 23:20:59 +0200

    Added DbContext

---
+++ Resume.Persistence/Data/ResumeDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Resume.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resume.Persistence.Data
+{
+    public class ResumeDbContext : DbContext
+    {
+        public DbSet<PaymentReceipt> PaymentReceipt { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
