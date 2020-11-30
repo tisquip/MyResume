1cce5c40a6d46d22dfc5bc6805b95714731e9611
Software Architects of Southern Africa (SASA) <soarsoaf@outlook.com>
2020-11-30 22:48:52 +0200

    Added Tests for domain (just as a sample) - Tests are failing (Red, Green Refactor)

---
+++ Resume.Domain.Tests/PaymentReceiptTests.cs
@@ -0,0 +1,41 @@
+using System;
+using Xunit;
+
+namespace Resume.Domain.Tests
+{
+    public class PaymentReceiptTests
+    {
+        
+        [Theory]
+        [InlineData(false, false)]
+        [InlineData(true, false)]
+        public void SetNotificationWasSentTest_AssesReturnedResult(bool notificationWasSet, bool expectedResultSucceeded)
+        {
+            //Arrange
+            PaymentReceipt paymentReceipt = new PaymentReceipt(new ValueObjects.Money(5));
+
+            //Act
+            var result = paymentReceipt.SetNotificationWasSent(notificationWasSet);
+            bool actualResultSucceeded = result.Succeeded;
+
+            //Assert
+            Assert.Equal(expectedResultSucceeded, actualResultSucceeded);
+        }
+
+        [Theory]
+        [InlineData(false, true)]
+        [InlineData(true, false)]
+        public void SetNotificationWasSentTest_AssessPropertyHasChanged(bool notificationWasSet, bool expectedNotificationPropertyValue)
+        {
+            //Arrange
+            PaymentReceipt paymentReceipt = new PaymentReceipt(new ValueObjects.Money(8));
+
+            //Act
+            var result = paymentReceipt.SetNotificationWasSent(notificationWasSet);
+            bool actualNotificationPropertyValue = paymentReceipt.NotificationWasSent;
+
+            //Assert
+            Assert.Equal(expectedNotificationPropertyValue, actualNotificationPropertyValue);
+        }
+    }
+}
