870a40eaa64943f52d211fa93a126f6e1ff75ac6
Software Architects of Southern Africa (SASA) <soarsoaf@outlook.com>
2020-12-05 22:30:44 +0200

    Added Xamarin Forms project

---
+++ Resume.Mob/Resume.Mob/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Resume.Mob.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Resume.Mob.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
